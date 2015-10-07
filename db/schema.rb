8acbc651cda3c30081ede48bcd7b1c043c9ba3be
isaacmccraw <isaacmccraw@rocketmail.com>
2015-10-06 22:34:40 -0400

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004002516) do
+ActiveRecord::Schema.define(version: 20151007021632) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "username"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
